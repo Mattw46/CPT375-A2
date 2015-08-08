48063506f77f59043bca7bae9941ed2ccaef3b8f
Mattw46 <matthew_w46@yahoo.com.au>
2015-08-08 18:54:24 +1000

    added sql query strings for login, is_admin and is_professional

--- model/model.php
+++ model/model.php
@@ -5,15 +5,48 @@
  * Used to perform functions of the web app*/
 require_once 'validate.php';
 
+define("INVALID_USER", 100);
+
 // returns user id if found else return 0
 function login($username, $password) {
 	if (validate_login($username, $password) == True) {
 		// query database
+		$sql = "SELECT * FROM USER 
+		WHERE username = $username;"; // add join to pword once populated
+		
+		// return $user_id if sql query successful
 		return 1;
 	}
 	else {
-		return 0;
+		return INVALID_USER;
 	}
+}
+
+// Returns True if user is a admin user
+function is_admin($user_id) {
+	// id of 0 and below indicates user is not authenticated
+	if ($user_id <= INVALID_USER) {
+		return false;
+	}
+	// query db for user code
+	$sql = "select user_id, username, user_typ.user_typ_cd, user_typ 
+			from user join user_typ on user.user_typ_cd=user_typ.user_typ_cd 
+			where user_id = 1;";
+			
+	// Query db and return True if user_typ is Admin
+}
+
+// Returns True if user is a professional
+function is_professional() {
+	if ($user_id <= INVALID_USER) {
+		return false;
+	}
+	// query db for user code
+	$sql = "select user_id, username, user_typ.user_typ_cd, user_typ 
+			from user join user_typ on user.user_typ_cd=user_typ.user_typ_cd 
+			where user_id = 1;";
+			
+	// Query db and return True if user_typ is Admin
 }
 
 function get_bids() {

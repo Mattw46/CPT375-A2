cd7c2074cf13f05133fd2d938e0984038e0e723e
Jalal Hobbs <jalalhobbs@gmail.com>
2015-08-16 05:36:38 +0930

    added titles to pages

--- addjob.php
+++ addjob.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+$pageTitle = "CPT375 A2 : Add Job";
 include_once("php/components/shared/header.php");
 include_once("php/components/content/addjob.php");
 include_once("php/components/shared/footer.php");
--- register.php
+++ register.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+$pageTitle = "CPT375 A2 : Register";
 include_once("php/components/shared/header.php");
 include_once("php/components/content/register.php");
 include_once("php/components/shared/footer.php");

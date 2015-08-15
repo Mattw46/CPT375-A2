9a85e6ff2ff9962daaa50f2da185cc7a42793160
Jalal Hobbs <jalalhobbs@gmail.com>
2015-08-16 04:38:01 +0930

    added post to register.php, added names, removed billing address

--- php/components/content/register.php
+++ php/components/content/register.php
@@ -1,105 +1,64 @@
-<form>
+<form action="./controller/controller.php" method="post">
     <div class="row content" id="registrationContent">
         <div class="col-xs-12 center-xs">
             <h2>Registration</h2>
         </div>
-
-
         <div class="row">
             <div class="col-xs-6 end-xs">
                 <div class="row">
                     <div class="col-xs-12">
                         <label for="usernameField">Username:</label>
-                        <input id="usernameField" type="text">
+                        <input id="usernameField" name="username" type="text">
                     </div>
                     <div class="col-xs-12">
                         <label for="firstNameField">First Name:</label>
-                        <input id="firstNameField" type="text">
+                        <input id="firstNameField" name="firstName" type="text">
                     </div>
                     <div class="col-xs-12">
                         <label for="lastNameField">Last Name:</label>
-                        <input id="lastNameField" type="text">
+                        <input id="lastNameField" name="lastName" type="text">
                     </div>
                     <div class="col-xs-12">
-                        <label for="addressField1">Address 1:</label>
-                        <input id="addressField1" type="text">
+                        <label for="address1Field">Address 1:</label>
+                        <input id="address1Field" name="address1" type="text">
                     </div>
                     <div class="col-xs-12">
-                        <label for="addressField2">Address 2:</label>
-                        <input id="addressField2" type="text">
+                        <label for="address2Field">Address 2:</label>
+                        <input id="address2Field" name="address2" type="text">
                     </div>
                     <div class="col-xs-12">
                         <label for="cityField">City:</label>
-                        <input id="cityField" type="text">
+                        <input id="cityField" name="city" type="text">
                     </div>
                     <div class="col-xs-12">
                         <label for="stateField">State:</label>
-                        <input id="stateField" type="text">
+                        <input id="stateField" name="state" type="text">
                     </div>
                     <div class="col-xs-12">
                         <label for="postCodeField">Post Code:</label>
-                        <input id="postCodeField" type="text">
+                        <input id="postCodeField" name="postCode" type="text">
                     </div>
 
                     <div class="col-xs-12">
                         <label for="emailField">Email:</label>
-                        <input id="emailField" type="text">
+                        <input id="emailField" name="email" type="text">
                     </div>
                     <div class="col-xs-12">
                         <label for="confirmEmailField">Confirm Email:</label>
-                        <input id="confirmEmailField" type="text">
+                        <input id="confirmEmailField" name="confirmEmail" type="text">
                     </div>
                     <div class="col-xs-12">
                         <label for="passwordField">Password:</label>
-                        <input id="passwordField" type="password">
+                        <input id="passwordField" name="password" type="password">
                     </div>
                     <div class="col-xs-12">
                         <label for="confirmPasswordField">Confirm Password:</label>
-                        <input id="confirmPasswordField" type="password">
+                        <input id="confirmPasswordField" name="confirmPassword" type="password">
                     </div>
                 </div>
-
-            </div>
-            <div class="col-xs-4 end-xs">
-                <div class="row end-xs">
-                    <div>
-                        <label for="billingAddressSame">Billing address is the same:</label>
-                        <input type="checkbox" id="billingAddressSame" name="sameAddress" value="sameAddress">
-                    </div>
-                    <section id="billingAddress">
-                        <div>
-                            <label for="billingFirstNameField">First Name:</label>
-                            <input id="billingFirstNameField" type="text">
-                        </div>
-                        <div>
-                            <label for="billingLastNameField">Last Name:</label>
-                            <input id="billingLastNameField" type="text">
-                        </div>
-                        <div>
-                            <label for="billingAddressField1">Address 1:</label>
-                            <input id="billingAddressField1" type="text">
-                        </div>
-                        <div>
-                            <label for="billingAddressField2">Address 2:</label>
-                            <input id="billingAddressField2" type="text">
-                        </div>
-                        <div>
-                            <label for="billingCityField">City:</label>
-                            <input id="billingCityField" type="text">
-                        </div>
-                        <div>
-                            <label for="billingStateField">State:</label>
-                            <input id="billingStateField" type="text">
-                        </div>
-                        <div>
-                            <label for="billingPostCodeField">Post Code:</label>
-                            <input id="billingPostCodeField" type="text">
-                        </div>
-                    </section>
-                </div>
-
             </div>
         </div>
+
         <div class="row">
 
         </div>

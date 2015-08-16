5d98e0f88524353fd7c75d72f9af62b2a5928c27
Jalal Hobbs <jalalhobbs@gmail.com>
2015-08-16 16:47:06 +0930

    if not logged in, redirect to log in

--- addjob.php
+++ addjob.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
-$pageTitle = "CPT375 A2 : Add Job";
-include_once("php/components/shared/header.php");
-include_once("php/components/content/addjob.php");
-include_once("php/components/shared/footer.php");
+if (isset($_SESSION['authenticated'])) {
+    $pageTitle = "CPT375 A2 : Add Job";
+    include_once("php/components/shared/header.php");
+    include_once("php/components/content/addjob.php");
+    include_once("php/components/shared/footer.php");
+} else {
+    header("location: ./login.php");
+}

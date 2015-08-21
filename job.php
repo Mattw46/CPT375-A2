67abf336ea01b5e785886a6f68af5620acce4f9d
Garret Bourke <bourkg@users.noreply.github.com>
2015-08-21 09:27:22 +0100

    Implement job user logged in

--- job.php
+++ job.php
@@ -1,6 +1,11 @@
 <?php
 //$jobid = getJobById(filter_var($_GET['job'],FILTER_SANITIZE_NUMBER_INT));
-$pageTitle = "CPT375 A2 : jobShortDescription";
-include_once("php/components/shared/header.php");
-include_once("php/components/content/job.php");
-include_once("php/components/shared/footer.php");
+session_start();
+if (isset($_SESSION['authenticated'])) {
+	$pageTitle = "CPT375 A2 : jobShortDescription";
+	include_once("php/components/shared/header.php");
+	include_once("php/components/content/job.php");
+	include_once("php/components/shared/footer.php");
+} else {
+    header("location: ./login.php");
+}

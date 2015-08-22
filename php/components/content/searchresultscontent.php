f3117ec5cf9939f2e7cb4cbe7473338123870ca4
Garret Bourke <bourkg@users.noreply.github.com>
2015-08-22 19:29:25 +0100

    Implemented search on categories on home page

--- php/components/content/searchresultscontent.php
+++ php/components/content/searchresultscontent.php
@@ -4,19 +4,18 @@
     if(isset($_GET['categoryId'])){
         $categoryId = $_GET['categoryId'];
         $allListingsByCategory = getJobListingByCategory($categoryId);
-     }
-
-
+    
         foreach($allListingsByCategory as $count => $db_query){ 
+           
             $db_array = getListingDetails($db_query[0][0]);
-            if($db_array['visible']) {?>
+            if($db_array[0]['visible']) {?>
                     <div class="col-xs-6 center-xs col-sm-4 col-lg-3 listing">
                         <div class="inner">
                             <img src="http://placehold.it/200x150">
 
-                            <p class="listing-title"><?php echo "Job Desc: " . $db_array['shrt_descn'] ?></p>
+                            <p class="listing-title"><?php echo "Job Desc: " . $db_array[0]['shrt_descn'] ?></p>
                             <?php 
-                                $currentb = getCurrentBid($db_array['listing_id']);
+                                $currentb = getCurrentBid($db_array[0]['listing_id']);
                                 if ($currentb == 0)
                                     $currentBid = 0;
                                 else
@@ -26,13 +25,13 @@
                          
                             <p class="listing-ends-in">Ends in: <span class="listing-ends-in-amount"><?php
                             $date1 = new DateTime(date('Y-m-d G:i:s'));
-                            $date2 = new DateTime($db_array['list_end_tmstmp']);
+                            $date2 = new DateTime($db_array[0]['list_end_tmstmp']);
                             $interval = $date1->diff($date2);
                             echo $interval->format("%d days %H:%I:%S"); ?></span></p>
 
                             <p class="listing-total-bids">Total Bids: <span class="listing-total-bids-amount">
                                  <?php 
-                                $totalB = getTotalBids($db_array['listing_id']);
+                                $totalB = getTotalBids($db_array[0]['listing_id']);
                                 if ($totalB[0][0] == NULL){
                                     $totalbid = 0;
                                 }
@@ -41,12 +40,14 @@
                                 echo $totalbid;
                                 ?>
                                 <form method="GET" action="./job.php">
-                                    <input type ="hidden" name = "listing_id" value = <?php echo $db_array['listing_id']; ?> />
+                                    <input type ="hidden" name = "listing_id" value = <?php echo $db_array[0]['listing_id']; ?> />
                                     <button type ="submot">Place bid</button>
                                 </form>
                             </span></p>
                         </div>
                     </div>
             <?php }
-                } ?>
+
+        } 
+    }?>
 </div>

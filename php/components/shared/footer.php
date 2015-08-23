3ef588b5ebfabd9ef49d207b5c25360048a5f2e9
tomble21 <s3384711@student.rmit.edu.au>
2015-08-23 21:47:30 +0930

    Update footer.php
    update footer

--- php/components/shared/footer.php
+++ php/components/shared/footer.php
@@ -1,6 +1,6 @@
 <div class="row" id="footer">
     <div class="col-xs-12">
-        <p>CPT375 - Web Database Applications - Assignment2 <a href="./php/components/content/auctionrules.php"> Auction Rules</a></p>
+        <p>CPT375 - Web Database Applications - Assignment2 <a href="./auctionrules.php"> Auction Rules</a></p>
     </div>
 </div>
 </div>
